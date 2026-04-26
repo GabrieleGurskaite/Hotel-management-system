10db9a61ea61d6bece094976053dfadb22c06405
GabrieleGurskaite <gurskaite.gabriele@gmail.com>
2026-04-26 08:32:44 +0300

    Add hotel data structure with rooms and reservations

---
+++ hotel_data.py
@@ -0,0 +1,24 @@
+{
+    "rooms": [
+        {
+            "number": 101,
+            "price": 50,
+            "available": true,
+            "type": "standard"
+        },
+        {
+            "number": 201,
+            "price": 80,
+            "available": true,
+            "type": "deluxe"
+        },
+        {
+            "number": 301,
+            "price": 120,
+            "available": true,
+            "type": "suite"
+        }
+    ],
+    "guests": [],
+    "reservations": []
+}
